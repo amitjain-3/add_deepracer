f2c68d0cf4b479027752d8218596ca90666cdd8b
Aymeric Wang <wang_aymeric@berkeley.edu>
2022-01-08 23:25:28 +0100

    stuck but reaching calculate_vel test pub

--- deepracer_follow_the_leader_ws/object_detection_pkg/object_detection_pkg/object_detection_node.py
+++ deepracer_follow_the_leader_ws/object_detection_pkg/object_detection_pkg/object_detection_node.py
@@ -422,7 +422,7 @@ class ObjectDetectionNode(Node):
                 Velocity.velocity = [vx,vy]
                 self.get_logger().info(f"Vel from target position: {vx},{vy}")
                 # self.get_logger().debug(f"Vel from target type: {type(vx)}")
-                # self.velocity_publisher.publish(Velocity)
+                self.velocity_publisher.publish(Velocity)
                 return (vx**2+vy**2)**0.5/(delta_t)
         except Exception as ex:
             self.get_logger().error(f"Failed velocity calculation step: {ex}")
