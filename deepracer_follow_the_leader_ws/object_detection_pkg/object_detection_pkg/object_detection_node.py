bd4e07881830b50b54fddcff25a67bc1316d19c6
Aymeric Wang <wang_aymeric@berkeley.edu>
2022-01-17 12:27:17 -0800

    removed exception that is empty

--- deepracer_follow_the_leader_ws/object_detection_pkg/object_detection_pkg/object_detection_node.py
+++ deepracer_follow_the_leader_ws/object_detection_pkg/object_detection_pkg/object_detection_node.py
@@ -389,13 +389,14 @@ class ObjectDetectionNode(Node):
             
             """ ADDED for thread creation """
             return(self.bottom_right_x,self.bottom_right_y,self.bb_center_x,self.bb_center_y)
-            
+        """    
         except Exception as ex:
             self.get_logger().error(f"Failed inference step: {ex}")
             # Destroy the ROS Node running in another thread as well.
             self.destroy_node()
             rclpy.shutdown()
-
+        """
+        
     """ ##################################################### """
     ### !!! IMPLEMENTING VELOCITY ESTIMATE WITH INTERPOLATION !!!
     """ ##################################################### """
