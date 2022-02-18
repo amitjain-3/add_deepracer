a23dda837a814d3135ea29a1b3934a15597bf1dc
Aymeric Wang <wang_aymeric@berkeley.edu>
2022-02-18 12:21:27 -0800

    install python deps

--- deepracer_follow_the_leader_ws/install_dependencies.sh
+++ deepracer_follow_the_leader_ws/install_dependencies.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+echo "Downloading Python libs"
+echo "[1/2] SmBus2..."
+pip3 install smbus2
+echo "[2/2] CVXPY..."
+pip3 install cvxpy
 echo "Cloning the async_web_server_cpp package"
 cd ~/deepracer_ws/add_deepracer/deepracer_follow_the_leader_ws
 git clone https://github.com/GT-RAIL/async_web_server_cpp.git
