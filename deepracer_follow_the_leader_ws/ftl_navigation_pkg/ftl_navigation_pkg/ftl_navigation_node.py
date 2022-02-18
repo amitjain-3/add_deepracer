fixed ego velocity calculation bug

deepracer_follow_the_leader_ws/ftl_navigation_pkg/ftl_navigation_pkg/ftl_navigation_node.py
@@ -101,7 +101,7 @@ class FTLNavigationNode(Node):
 
         # Create MPC controller and necessary variables
         self.MPC = deepracer_MPC.MPC()
-        self.prev_ego_accel = 0
+        self.prev_ego_speed = 0
         self.front_velocity_subscriber = self.create_subscription(ObjVelocityMsg,
                                                        constants_obj.INTERPOLATION_VELOCITY_PUBLISHER_TOPIC,
                                                        self.get_front_velocity,
@@ -191,8 +191,8 @@ class FTLNavigationNode(Node):
         # ego vehicle speed
         accel_data,gyro_data = self.get_imu_data()
         self.get_logger().info(f"Accelerometer data:{accel_data} gyro data: {gyro_data}")
-        ego_speed = (accel_data[0] - self.prev_ego_accel)/0.1
-        self.prev_ego_accel = accel_data
+        ego_speed = self.prev_ego_speed + accel_data[0]*0.1
+        self.prev_ego_speed = ego_speed
 
         # construct state vector
         x_t = np.array([[car_dist],
@@ -230,14 +230,14 @@ class FTLNavigationNode(Node):
     def get_sim_MPC_action(self, car_dist):
         # if first step of sim, set initial values 
         if self.prev_ego_accel == [0, 0, 0]:
-            self.MPC.v_f = 1
+            self.MPC.v_f = 1 # starting speed of "phamtom" front car in m/s
 
         # get current ego vehicle speed
         accel_data,gyro_data = self.get_imu_data()
 
         self.get_logger().info(f"Accelerometer data:{accel_data} gyro data: {gyro_data}")
-        ego_speed = (accel_data[0] - self.prev_ego_accel)/0.1
-        self.prev_ego_accel = accel_data
+        ego_speed = self.prev_ego_speed + accel_data[0]*0.1
+        self.prev_ego_speed = ego_speed
 
         # construct state vector
         x_t = np.array([[car_dist],
@@ -247,7 +247,7 @@ class FTLNavigationNode(Node):
         # Step MPC with current state
         [feas, x_opt, u_opt, J_opt] = self.MPC.MPC_step(x_t)
         torque = u_opt.value[0][0]
-        self.get_logger().info(f"Before MPC step:{ego_speed}")
+        self.get_logger().info(f"After MPC step:{ego_speed}")
 
         # calculate new distance between cars and slow down "phantom" front car
         car_dist += (self.MPC.v_f - ego_speed)*0.1